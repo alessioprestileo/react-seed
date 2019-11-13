76c930566cf1ca15089cb07165583d2536721cf0
ramu15391 <ramamurthi@skava.com>
2019-11-14 00:59:21 +0530

    search added

--- src/components/movieListPage.tsx
+++ src/components/movieListPage.tsx
@@ -3,21 +3,33 @@ import { getListView } from './ListView';
 
 const Movie = () => {
   const [movie, setMovie] = React.useState(false);
+  const [searchTerm, setSearchTerm] = React.useState('fast');
   const getData = async () => {
-    const data = await fetch(
-      'https://www.omdbapi.com/?s=avengers&type=movie&apikey=85bd3106'
-    );
-    const movieData = await data.json();
-    setMovie(movieData.Search);
+    if (searchTerm !== '') {
+      const data = await fetch(
+        `https://www.omdbapi.com/?s=${searchTerm}&type=movie&apikey=85bd3106`,
+      );
+      const movieData = await data.json();
+      setMovie(movieData.Search);
+    }
+  };
+
+  const getMovieSearchText = (e:any) => {
+    setSearchTerm(e.currentTarget.value);
+  };
+  const onSearchSubmit = () => {
+    getData();
   };
 
   React.useEffect(() => {
     getData();
-  }, []);
+  },              []);
 
   return (
     <div className="App">
       <h1>Movie List</h1>
+      <input onChange={getMovieSearchText} type="text" />
+      <button onClick={onSearchSubmit}>Submit</button>
       {getListView(movie)}
     </div>
   );
