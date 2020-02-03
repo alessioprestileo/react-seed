665e4686c183e72ace65830daba890a3da218243
Aravindan R <ramaravind000@gmail.com>
2020-02-03 18:56:20 +0530

    Align Css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -12,15 +12,10 @@ import ListView from './reports/ListView';
 import Detailview from './reports/Detailview';
 
 const App = () => {
-   // const appTitle = 'This is a seed for a React project';
-
   return (
     <Router>
     <div className="app">
-      {/* <header className="app-header">
-        <h1 className="app-title">{appTitle}</h1>
-      </header>
-      <div className="page">Insert the app content here.</div> */}
+      <div className="page">
       <Switch>
         <Route exact path="/">
           <ListView />
@@ -29,6 +24,7 @@ const App = () => {
           <Detailview />
         </Route>
       </Switch>
+      </div>
     </div>
     </Router>
   );
